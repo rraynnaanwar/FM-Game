43225ee6c675c0b3bd6118aba4e148bcad63f348
rraynaanwar <rraynaanwar@gmail.com>
2024-06-22 23:18:35 -0400

    data has been written into a file

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import sys
+import os
 from DataScraper import DataScraper
 from Database import DataBase
 sys.stdout.reconfigure(encoding='utf-8')
 
 scraper = DataScraper()
 dataMap = scraper.run()
-for key, val in dataMap.items():
-    print(key)
-    playersArray = val
-    for player in playersArray:
-        print(player)
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+filePath = os.path.join(script_dir, "players.txt")
+
+with open(filePath, 'w', encoding='utf-8') as file:
+    for key, val in dataMap.items():
+
+        file.write(f"{key}\n")
+        playersArray = val
+
+        for player in playersArray:
+            file.write(f"{player}\n")
